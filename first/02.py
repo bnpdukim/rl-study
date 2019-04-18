fe28e305d346cd8569a22721617db7d8a2cd2b7a
bnpdukim <bnpdukim@bnpinnovation.com>
2019-04-18 15:11:49 +0900

    nn

--- first/02.py
+++ first/02.py
@@ -10,5 +10,23 @@ learning_rate = 0.01
 X = tf.placeholder(tf.float32, shape=[None, n_inputs])
 hidden = tf.layers.dense(X, n_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
 logits = tf.layers.dense(hidden, n_outputs, kernel_initializer=initializer)
-outpus = tf.nn.sigmoid(logits)
-p_left_and_right = tf.concat
+outputs = tf.nn.sigmoid(logits)
+p_left_and_right = tf.concat(axis=1, values=[outputs, 1-outputs])
+action = tf.multinomial(tf.log(p_left_and_right), num_samples=1)
+
+y = 1. - tf.to_float(action)
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+grads_and_vars = optimizer.compute_gradients(cross_entropy)
+gradients = [grad for grad, variable in grads_and_vars]
+gradient_placeholders = []
+grads_and_vars_feed = []
+
+for grad, variable in grads_and_vars:
+  gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+  gradient_placeholders.append(gradient_placeholder)
+  grads_and_vars_feed.append((gradient_placeholder, variable))
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
